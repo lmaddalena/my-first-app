b262bda873ddd6097ace4058a666a7a0cfd49922
lmaddalena <mdn.luca@gmail.com>
2019-05-17 16:58:27 +0200

    Up to module 2.38

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class ServerComponent {
+    serverID = 10;
+    serverStatus = 'offline';
 
+    constructor() {
+        this.serverStatus = Math.random() >= .5 ? 'offline' : 'online';
+    }
+    getServerStatus() {
+        return this.serverStatus;
+    }
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
+    }
 }
